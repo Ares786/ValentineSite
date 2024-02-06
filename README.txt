e039ef70f21ea97c2ccc684133e69818c88ecfc7
Talhah <84022672+Ares786@users.noreply.github.com>
2024-02-06 06:43:27 +0000

    Valentinesite

---
+++ README.txt
@@ -0,0 +1,14 @@
+Hi, 
+
+I made this website for my girlfriend asking her to be my valentine.
+I'm self-taught and everything here is just stuff i picked up and learnt picking apart
+other peoples code and alot of trial and error.
+
+I aim to make my work from here beginner friendly to help alot of new coders to share this journey with me. 
+Specifically in this code i've added text lines briefly mentioning what a set line of code is doing i've added some extra 
+images and sounds feel free to use your own (make sure to update the file path in the html and JS)
+
+There may have been quicker ways and shortcuts but to learn everything must be broken down 
+so hopefully the code is easy to follow along to!
+
+Thanks, Ares <3
---
+++ index2.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const noButton = document.getElementById('nobutton');
+    const yesButton = document.getElementById('yesbutton');
+    const secondNoButton = document.getElementById('second-no-button');
+    const secondYesButton = document.getElementById('second-yes-button');
+    const thirdNoButton = document.getElementById('third-no-button');
+    const thirdYesButton = document.getElementById('third-yes-button')
+    const mainQuestion = document.querySelector('.Mainquestion');
+    const noMessage = document.getElementById('no-message');
+    const yesMessage = document.getElementById('yes-message');
+    const secondNoMessage = document.getElementById('secondnomessage')
+    const mainImage = document.querySelector('.mainimage');
+    const noChoice = document.getElementById('nochoice')
+    const awkwardText = document.getElementById('awkwardtext')
+    const yesSound = document.getElementById('amidreaming');
+    const idea22 = document.getElementById('idea')
+    ///all of the above is to apply a const to each element ID from the html you can identify everything easier 
+
+    yesSound.volume = 0.3
+    idea22.volume = 0.2
+
+
+    // moves the 3rd no button when pressed 
+    function moveButton(button) {
+        const maxX = window.innerWidth - button.offsetWidth;
+        const maxY = window.innerHeight - button.offsetHeight;
+        const randomX = Math.floor(Math.random() * maxX)
+        const randomY = Math.floor(Math.random() * maxY);
+        button.style.position = 'fixed'; // Using 'fixed' to position relative to the viewport
+        button.style.left = `${randomX}px`;
+        button.style.top = `${randomY}px`;
+
+    }
+
+
+  noButton.addEventListener('click', function() {
+      mainQuestion.style.display = 'none';
+      noMessage.style.display = 'block';
+      mainImage.src = "images/angrycat.jpg"
+      idea22.play();
+    });
+
+    yesButton.addEventListener('click', function() {
+      mainQuestion.style.display = 'none';
+      yesMessage.style.display = 'block';
+      mainImage.src = 'images/celebrationgif.gif';
+      yesSound.play(); ///plays the yessound when yes is clicked 
+    
+    });
+  
+    secondNoButton.addEventListener('click', function() {
+        noMessage.style.display = 'none';
+        secondNoMessage.style.display = 'block';
+        noChoice.style.display = 'none'; ///this makes sure the nochoice txt doesn't show until the thirdnobutton click
+        mainImage.src = "images/sideeye.jpeg"
+    });
+  
+    secondYesButton.addEventListener('click', function() {
+      noMessage.style.display = 'none';
+      yesMessage.style.display = 'block';
+      mainImage.src = 'images/celebrationgif.gif';
+      yesSound.play();
+      idea22.pause(); ///as soon as second yes is clicked we want the sat music to stop
+    });
+
+    ///i am directing the third no button to carry 
+    ///the moveButton function when it's clicked 
+    thirdNoButton.addEventListener('click', function() {
+        moveButton(this);
+        noChoice.style.display = 'block';
+        awkwardText.style.display = 'none';
+
+
+    });
+
+
+    thirdYesButton.addEventListener('click', function() {
+        secondNoMessage.style.display = 'none';
+        yesMessage.style.display = 'block';
+        noChoice.style.display = 'none';
+        yesSound.play();
+        idea22.pause();
+
+    });     
+
+});
+
+
+
+  
